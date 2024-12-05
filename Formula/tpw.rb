b0186be8ec5542b81d86d83c7dc135aa9a1a3c75
lzt1008 <lzt1008@live.com>
2024-12-05 22:13:43 +0800

    chore: update tpw.rb for release 0.1.1

--- Formula/tpw.rb
+++ Formula/tpw.rb
@@ -1,10 +1,10 @@
 class Tpw < Formula
-  version "0.1.0"
+  version "0.1.1"
 
   desc "A lightweight macOS command-line tool for monitoring power usage"
   homepage "https://github.com/lzt1008/tpw"
   url "https://github.com/lzt1008/tpw/releases/download/v#{version}/tpw-v#{version}.tar.gz"
-  sha256 "da84673156ae7cb54f9287d468c4808697ee5045717dc1bd9d562aa7d2e700ba"
+  sha256 "6f4b033b589ca3a9ec525b5bbfd37ed0947be81ecd31e705b66271b50714ae37"
 
   def install
     bin.install "tpw"
